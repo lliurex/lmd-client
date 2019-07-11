2cd00499249b339ff961285117ca7909029c5571
juanma1980 <juanma1980@gmail.com>
2019-07-11 10:09:22 +0200

    fix sessions

--- install/usr/sbin/awesome-desktop.sh
+++ install/usr/sbin/awesome-desktop.sh
@@ -5,6 +5,6 @@ export HOME=/root
 #eval "( awesome --config /var/lib/ltsp-awesome-desktop/rc.lua ) &"
 eval "( setxkbmap -model pc105 -rules evdev -layout es -variant cat ) &"
 eval "( jwm ) &"
-eval "( feh --bg-scale /usr/share/lliurex-grub-custom/backgrounds/trusty.png ) &"
+eval "( feh --bg-scale /usr/share/awesome/themes/lliurex/back-awesome.png ) &"
 eval "( idesk )"
 
