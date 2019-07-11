19a16d478cac0af9f5061c53ae59f480a0afb4fe
juanma1980 <juanma1980@gmail.com>
2019-07-11 09:32:52 +0200

    update sessions and shells

--- install/usr/sbin/mate-ltsp-desktop.sh
+++ install/usr/sbin/mate-ltsp-desktop.sh
@@ -2,11 +2,5 @@
 
 export HOME=/root
 eval "( setxkbmap -model pc105 -rules evdev -layout es -variant cat ) &"
-eval "( mate-session ) &"
-eval "( marco --replace )"
-
-#eval "( awesome --config /var/lib/ltsp-awesome-desktop/rc.lua ) &"
-#eval "( jwm ) &"
-#eval "( feh --bg-scale /usr/share/lliurex-grub-custom/backgrounds/trusty.png ) &"
-#eval "( idesk )"
+eval "( dbus-launch /usr/bin/startkde ) "
 
