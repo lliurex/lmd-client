199cab121e707083df3ac329b03753a596ec9284
juanma1980 <juanma1980@gmail.com>
2019-07-11 12:26:10 +0200

    Wait until display is really available

--- install/usr/sbin/ltsp_edit_command.sh
+++ install/usr/sbin/ltsp_edit_command.sh
@@ -3,5 +3,13 @@
 export HOME=/root
 export LANG="es_ES.UTF-8"
 
+a=0
+while [[ ! -z $(xhost -) ]]
+do 
+	let a=$a+1
+	[[ $a -gt 10 ]] && break
+	sleep 1
+done
+
 eval "( setxkbmap -model pc105 -rules evdev -layout es -variant cat ) &"
 eval "( $@ )"
